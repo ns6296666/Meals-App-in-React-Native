ed946626935944926852e1d6cf2d0e6d5b147b7c
Harry <harry@macbookpro>
2024-01-27 20:00:55 +0000

    display meals

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealsOverview({ route }) {
   const catId = route.params.categoryId;
-  console.log("catId======", route);
+  const displayMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.includes(catId);
+  });
+  const renderMeal = (renderItem) => {
+    console.log("renderItem", renderItem);
+    return <MealItem title={renderItem.item.title} />;
+  };
   return (
     <View>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        keyExtractor={(itemData) => itemData.id}
+        renderItem={renderMeal}
+        data={displayMeals}
+      />
     </View>
   );
 }
