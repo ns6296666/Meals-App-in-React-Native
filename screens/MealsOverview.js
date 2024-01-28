477f3f45a35ff6ca58ee7c430c4b282e5a718952
Harry <harry@macbookpro>
2024-01-28 12:46:55 +0000

    setting option dynamically

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { StyleSheet, View, Text, FlatList } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-function MealsOverview({ route }) {
+function MealsOverview({ route, navigation }) {
   const catId = route.params.categoryId;
   const displayMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.includes(catId);
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+    navigation.setOptions({ title: categoryTitle });
+  }, [catId, navigation]);
+
   const renderMeal = (renderItem) => {
     const item = renderItem.item;
     const mealItemProp = {
